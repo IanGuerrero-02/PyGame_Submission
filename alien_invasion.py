d894dfbe59af4f2bb62a63329c4a7649b4dc2556
Ian Guerrero <ian_guerrero1@baylor.edu>
2025-06-25 11:48:21 -0500

    ED.3

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,18 @@
-import sys
+#import sys
 
 import pygame
+
+from settings import Settings
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
 
     def __init__(self):
         #"""Initialize the game, and create game resources."""
-        #pygame.init()
-        
+        pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.bg_color = (230,230,230)
@@ -18,15 +20,15 @@ class AlienInvasion:
     def run_game(self):
         #"""Start the main loop for the game."""
         while True:
-            #Watch for keyboard anbd mouse events.
-            for event in pygame.event.get():
-                if event.type ==pygame.QUIT:
-                    sys.exit()
+            #Watch for keyboard and mouse events.
+            #for event in pygame.event.get():
+                #if event.type ==pygame.QUIT:
+                    #sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
                 
 
             pygame.display.flip()
-if __name__=='__main__':
-    ai = AlienInvasion()
-    ai.run_game
+#if __name__=='__main__':
+ #   ai = AlienInvasion()
+  #  ai.run_game
