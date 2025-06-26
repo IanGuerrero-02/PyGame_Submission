c190ca995de02b05ca2c54dc8a63cb0698421881
Ian Guerrero <ian_guerrero1@baylor.edu>
2025-06-26 16:55:55 -0500

    Dropping the Fleet and Changing Direction

--- alien.py
+++ alien.py
@@ -21,7 +21,7 @@ class Alien(Sprite):
     # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
 
-        def check_edges(self):
+    def check_edges(self):
             """Return True if alien is at edge of screen."""
             screen_rect = self.screen.get_rect()
             if self.rect.right >= screen_rect.right or self.rect.left <=0:
