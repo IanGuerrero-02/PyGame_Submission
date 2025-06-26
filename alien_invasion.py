c50c8a0323a02915f17c5d694bcad9b8daf1d7ba
Ian Guerrero <ian_guerrero1@baylor.edu>
2025-06-26 12:33:56 -0500

    ED.8

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -32,6 +33,9 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN: 
                 if event.key == pygame.K_RIGHT: 
                     self.ship.moving_right = True
+            elif event.type ==pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
                 elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
