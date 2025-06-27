03afe144351c5d5ab7191cd21b7c8be35601628c
Ian Guerrero <ian_guerrero1@baylor.edu>
2025-06-26 19:09:19 -0500

    High Score

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,8 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        #High score should never be reset.
+        self.high_score = 0
     #Start Alien Invasion in an active state 
         self.game_active = True 
 
